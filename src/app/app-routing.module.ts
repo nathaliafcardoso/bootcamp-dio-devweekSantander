511ee0e5d9fcf2a96ef64a3c3d20fb69fd932c8b
Your Name <you@example.com>
2021-06-03 03:09:56 -0300

    Frontend Bootcamp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { DashboardPageComponent } from './dashboard/dashboard-page/dashboard-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
